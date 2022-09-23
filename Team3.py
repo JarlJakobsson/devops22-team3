cccfd0df7493d3c5f331fc96ff54646303aa6348
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2022-09-23 16:58:06 +0200

    removed menu class

--- Team3.py
+++ Team3.py
@@ -1,82 +1,82 @@
-#import json
-import sqlite3
-#from Rawdata import persons
-#import menufile
-#import functions
+# #import json
+# import sqlite3
+# #from Rawdata import persons
+# #import menufile
+# #import functions
 
-# try:
-#     with open(input("Enter path: "), 'x') as f:
-#         f.write(json.dumps(persons, indent=4))
+# # try:
+# #     with open(input("Enter path: "), 'x') as f:
+# #         f.write(json.dumps(persons, indent=4))
 
-# except: 
-#     print("File already exist.")
+# # except: 
+# #     print("File already exist.")
 
-class Person():
-    def __init__(self, firstname, lastname, birthyear, address):
-        self.firstname = firstname
-        self.lastname = lastname
-        self.birthyear = birthyear
-        self.address = address
+# class Person():
+#     def __init__(self, firstname, lastname, birthyear, address):
+#         self.firstname = firstname
+#         self.lastname = lastname
+#         self.birthyear = birthyear
+#         self.address = address
 
-    def __str__(self):
-        return self.lastname
+#     def __str__(self):
+#         return self.lastname
     
-    def __repr__(self) -> str:
-         return self.firstname
+#     def __repr__(self) -> str:
+#          return self.firstname
 
-CREATE_TABLE_PERSON = '''
-                CREATE TABLE IF NOT EXISTS person(
-                    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                    firstname TEXT,
-                    lastname TEXT,
-                    birthyear INTEGER,
-                    address TEXT
-                )
-                '''
+# CREATE_TABLE_PERSON = '''
+#                 CREATE TABLE IF NOT EXISTS person(
+#                     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+#                     firstname TEXT,
+#                     lastname TEXT,
+#                     birthyear INTEGER,
+#                     address TEXT
+#                 )
+#                 '''
 
-INSERT_DATA = '''
-            INSERT INTO person(
-                firstname,
-                lastname,
-                birthyear,
-                address
-            )
-            VALUES (?, ?, ?, ?)
-            '''
-def start_loop():
-        running = True
-        while running:
-            print(menufile.Menu().meny_text)
-            choice = menufile.Menu().user_choice()
-            menufile.Menu().menu_commands(choice)
-            menufile.Menu().wait_for_user()
-start_loop()
+# INSERT_DATA = '''
+#             INSERT INTO person(
+#                 firstname,
+#                 lastname,
+#                 birthyear,
+#                 address
+#             )
+#             VALUES (?, ?, ?, ?)
+#             '''
+# def start_loop():
+#         running = True
+#         while running:
+#             print(meny_text)
+#             choice = menufile.Menu().user_choice()
+#             menufile.Menu().menu_commands(choice)
+#             menufile.Menu().wait_for_user()
+# start_loop()
 
-#ÄNDRA memory = name of database
-with sqlite3.connect(':memory:', isolation_level=None) as sql_connection:
-    sql_connection.execute(CREATE_TABLE_PERSON)
+# #ÄNDRA memory = name of database
+# with sqlite3.connect(':memory:', isolation_level=None) as sql_connection:
+#     sql_connection.execute(CREATE_TABLE_PERSON)
 
 
 
-#'databas.db'
+# #'databas.db'
 
-#ÄNDRA userinput what file to read and create a DB of
-# with open("jsonpersons.json") as f:
-#     json_persons = json.load(f)
-#     mydudes = []
-#     for p in functions.['persons']:
-#         mydudes.append(Person(p['firstname'], p['lastname'], p['birth'], p['adress']))
-#         sql_connection.execute(INSERT_DATA, tuple(p.values()))
+# #ÄNDRA userinput what file to read and create a DB of
+# # with open("jsonpersons.json") as f:
+# #     json_persons = json.load(f)
+# #     mydudes = []
+# #     for p in functions.['persons']:
+# #         mydudes.append(Person(p['firstname'], p['lastname'], p['birth'], p['adress']))
+# #         sql_connection.execute(INSERT_DATA, tuple(p.values()))
 
-# print("---------------------DUDES INC--------------------")
-# print(mydudes)
-# print("---------------------DUDES OUT--------------------")
+# # print("---------------------DUDES INC--------------------")
+# # print(mydudes)
+# # print("---------------------DUDES OUT--------------------")
 
-def list_all():
-    cursor = sql_connection.cursor()
-    cursor.execute('SELECT * FROM person')
-    sql_data = cursor.fetchall()
-    for e in sql_data:
-        print(e)
+# def list_all():
+#     cursor = sql_connection.cursor()
+#     cursor.execute('SELECT * FROM person')
+#     sql_data = cursor.fetchall()
+#     for e in sql_data:
+#         print(e)
 
-sql_connection.close()
+# sql_connection.close()
--- menufile.py
+++ menufile.py
@@ -1,11 +1,9 @@
 import json
 import sqlite3
 
+running = True
 
-
-
-class Menu:
-    menu_text = """
+menu_text = """
 
     1. Load data from file
     2. Print persons from database
@@ -16,158 +14,152 @@ class Menu:
     type q or Q to exit
     """
 
-    def user_choice(self):
+def user_choice():
+    if running == True:
         return input("Enter your choice: ")
 
-    def wait_for_user(self):
-        if self.running:
-            input("Please press any key to continues.")
+def wait_for_user():
+        input("Please press any key to continues.")
 
-    def start_loop(self):
-        self.running = True
-        while self.running:
-            print(Menu.menu_text)
-            choice = self.user_choice()
-            self.menu_commands(choice)
-            self.wait_for_user()
+def start_loop():
+    # running = True
+    while running:
+        print(menu_text)
+        choice = user_choice()
+        menu_commands(choice)
+        wait_for_user()
 
-    def menu_commands(self, choice):
-        printbool = False
-        if choice == "q" or choice == "Q":
-            self.running = False
+def menu_commands(choice):
+    if choice == "q" or choice == "Q":
+        return running == False
 
-        elif choice == "1":
-            printbool = True
-            with open("jsonpersons.json") as f:
-                json_persons = json.load(f)
-                person_list = []
-                for p in json_persons["persons"]:
-                    person_list.append(Person(p["firstname"], p["lastname"], p["birth"], p["address"]))
-                    sql_connection.execute(INSERT_DATA, tuple(p.values()))
-            print("*** DATABASE LOADED ***")
+    elif choice == "1":
+        printbool = True
+        with open("jsonpersons.json") as f:
+            json_persons = json.load(f)
+            person_list = []
+            for p in json_persons["persons"]:
+                person_list.append(Person(p["firstname"], p["lastname"], p["birth"], p["address"]))
+                sql_connection.execute(INSERT_PERSON_DATA, tuple(p.values()))
+        print("*** DATABASE LOADED ***")
 
-        ### SECOND MENU START ###
-        elif choice == "2":
-            print(
-                """
+    ### SECOND MENU START ###
+    elif choice == "2":
+        print(
+            """
 
-    What do you want to print for
+What do you want to print for
 
-    1. Firstname
-    2. Lastname
-    3. Birthyear
-    4. Address
-    5. Print all
-    6. Print all persons with a hobby
-    
-    type q or Q to return to mainmenu
-    """
-            )
+1. Firstname
+2. Lastname
+3. Birthyear
+4. Address
+5. Print all
+6. Print all persons with a hobby
 
-            print_choice = input("Enter choice: ")
-            if print_choice == "q" or choice == "Q":
-                print("*** Returning to mainmenu ***")
+type q or Q to return to mainmenu
+"""
+        )
 
-            elif print_choice == "1":
-                list_all()
-                firstname_input = input("Enter the first name: ").lower()
-                cursor = sql_connection.cursor()
-                cursor.execute(
-                    f'SELECT * FROM person WHERE firstname = "{firstname_input}"'
-                )
-                sql_data = cursor.fetchall()
-                for e in sql_data:
-                    print(e)
+        print_choice = input("Enter choice: ")
+        if print_choice == "q" or choice == "Q":
+            print("*** Returning to mainmenu ***")
 
-            elif print_choice == "2":
-                list_all()
-                lastname_input = input("Enter the last name: ").lower()
-                cursor = sql_connection.cursor()
-                cursor.execute(
-                    f'SELECT * FROM person WHERE lastname = "{lastname_input}"'
-                )
-                sql_data = cursor.fetchall()
-                for e in sql_data:
-                    print(e)
-
-            elif print_choice == "3":
-                list_all()
-                birth_input = int(
-                    input("Enter the birthyear of the person you want to printout: ")
-                )
-                cursor = sql_connection.cursor()
-                cursor.execute(f'SELECT * FROM person WHERE birth = "{birth_input}"')
-                sql_data = cursor.fetchall()
-                for e in sql_data:
-                    print(e)
-
-            elif print_choice == "4":
-                list_all()
-                address_input = input("Enter the address of the person: ").lower()
-                cursor = sql_connection.cursor()
-                cursor.execute(
-                    f'SELECT * FROM person WHERE address = "{address_input}"'
-                )
-                sql_data = cursor.fetchall()
-                for e in sql_data:
-                    print(e)
-                input("Press any key to continue... ")
-
-            elif print_choice == "5":
-                try:
-                    if printbool == False:
-                        print("Nothing to print")
-                    else:
-                        print("*** HERE IS ALL DATA ***")
-                        list_all()
-                except:
-                    print("Nothing to print")
-            
-            elif print_choice == "6":
-                try:
-                    cursor = sql_connection.cursor()
-                    cursor.execute('''SELECT firstname, hobbyname 
-                                    FROM person AS p 
-                                    INNER JOIN hobby AS h 
-                                    ON p.id = h.personid
-                                    ORDER BY firstname
-                    ''')
-                    sql_data = cursor.fetchall()
-                    for e in sql_data:
-                        print(e)
-                except:
-                    except_msg()
-
-                ### SECOND MENU END ###
-
-        elif choice == "3":
-            try:
-                list_all()
-                userinput = input("\nWho do you want to delete? Enter ID: ")
-                sql_connection.execute(f"DELETE FROM person WHERE id = '{userinput}'")
-                print(f"Person {userinput} has been deleted")
-            except:
-                except_msg()
-
-        elif choice == "4":
-            try:
-                list_all()
-                userinput = input("\nWhos address do you want to Update? Enter ID: ")
-                sql_connection.execute(f"SELECT * FROM person WHERE id = '{userinput}'")
-                new_adress = input("\nEnter the new address: ")
-                sql_connection.execute(f"UPDATE person SET address = '{new_adress}' WHERE id = '{userinput}'")
-            except:
-                except_msg()
-        
-        elif choice == "5":
+        elif print_choice == "1":
             list_all()
+            firstname_input = input("Enter the first name: ").lower()
+            cursor = sql_connection.cursor()
+            cursor.execute(
+                f'SELECT * FROM person WHERE firstname = "{firstname_input}"'
+            )
+            sql_data = cursor.fetchall()
+            for e in sql_data:
+                print(e)
+
+        elif print_choice == "2":
+            list_all()
+            lastname_input = input("Enter the last name: ").lower()
+            cursor = sql_connection.cursor()
+            cursor.execute(
+                f'SELECT * FROM person WHERE lastname = "{lastname_input}"'
+            )
+            sql_data = cursor.fetchall()
+            for e in sql_data:
+                print(e)
+
+        elif print_choice == "3":
+            list_all()
+            birth_input = int(
+                input("Enter the birthyear of the person you want to printout: ")
+            )
+            cursor = sql_connection.cursor()
+            cursor.execute(f'SELECT * FROM person WHERE birth = "{birth_input}"')
+            sql_data = cursor.fetchall()
+            for e in sql_data:
+                print(e)
+
+        elif print_choice == "4":
+            list_all()
+            address_input = input("Enter the address of the person: ").lower()
+            cursor = sql_connection.cursor()
+            cursor.execute(
+                f'SELECT * FROM person WHERE address = "{address_input}"'
+            )
+            sql_data = cursor.fetchall()
+            for e in sql_data:
+                print(e)
+            input("Press any key to continue... ")
+
+        elif print_choice == "5":
+            print("*** HERE IS ALL DATA ***")
+            list_all()
+        
+        elif print_choice == "6":
             try:
-                userid = int(input("Enter ID of person to add hobby: "))
-                input_hobby = input("Enter the name of the hobby: ")
-                sql_connection.execute(INSERT_HOBBY, (userid, input_hobby))
-                list_hobbies()
+                cursor = sql_connection.cursor()
+                cursor.execute('''SELECT firstname, lastname, birth, address, hobbyname 
+                                FROM person AS p 
+                                INNER JOIN hobby AS h 
+                                ON p.id = h.personid
+                                ORDER BY firstname
+                ''')
+                sql_data = cursor.fetchall()
+                for e in sql_data:
+                    print(e)
             except:
                 except_msg()
+
+            ### SECOND MENU END ###
+
+    elif choice == "3":
+        try:
+            list_all()
+            userinput = input("\nWho do you want to delete? Enter ID: ")
+            sql_connection.execute(f"DELETE FROM person WHERE id = '{userinput}'")
+            print(f"Person {userinput} has been deleted")
+            person_list
+        except:
+            except_msg()
+
+    elif choice == "4":
+        try:
+            list_all()
+            userinput = input("\nWhos address do you want to Update? Enter ID: ")
+            sql_connection.execute(f"SELECT * FROM person WHERE id = '{userinput}'")
+            new_adress = input("\nEnter the new address: ")
+            sql_connection.execute(f"UPDATE person SET address = '{new_adress}' WHERE id = '{userinput}'")
+        except:
+            except_msg()
+    
+    elif choice == "5":
+        list_all()
+        try:
+            userid = int(input("Enter ID of person to add hobby: "))
+            input_hobby = input("Enter the name of the hobby: ")
+            sql_connection.execute(INSERT_HOBBY_DATA, (userid, input_hobby))
+            list_hobbies()
+        except:
+            except_msg()
 
 class Person:
     def __init__(self, firstname, lastname, birthyear, address):
@@ -196,24 +188,24 @@ CREATE_TABLE_PERSON = """
                 """
 
 CREATE_TABLE_HOBBIES = '''
-            CREATE TABLE IF NOT EXISTS hobby(
-                hobbyid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                personid INTEGER NOT NULL,
-                hobbyname   TEXT NOT NULL,
-            FOREIGN KEY(personid) REFERENCES person(id)
-            )'''
+                CREATE TABLE IF NOT EXISTS hobby(
+                    hobbyid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                    personid INTEGER NOT NULL,
+                    hobbyname   TEXT NOT NULL,
+                FOREIGN KEY(personid) REFERENCES person(id)
+                )'''
 
-INSERT_DATA = """
-            INSERT INTO person(
-                firstname,
-                lastname,
-                birth,
-                address
-            )
-            VALUES (?, ?, ?, ?)
-            """
+INSERT_PERSON_DATA = """
+                INSERT INTO person(
+                    firstname,
+                    lastname,
+                    birth,
+                    address
+                )
+                VALUES (?, ?, ?, ?)
+                """
 
-INSERT_HOBBY = """
+INSERT_HOBBY_DATA = """
             INSERT INTO hobby(
                 personid,
                 hobbyname
@@ -224,16 +216,14 @@ INSERT_HOBBY = """
 with sqlite3.connect(":memory:", isolation_level=None) as sql_connection:
     sql_connection.execute(CREATE_TABLE_PERSON)
     sql_connection.execute(CREATE_TABLE_HOBBIES)
+    cursor = sql_connection.cursor()
 
 def list_all():
     cursor = sql_connection.cursor()
     cursor.execute("SELECT * FROM person")
     sql_data = cursor.fetchall()
     for e in sql_data:
-        if bool(e) == True:
-            print("Database empty")
-        else:
-            print(e)
+        print(e)
 
 def list_hobbies():
     cursor = sql_connection.cursor()
@@ -245,7 +235,7 @@ def list_hobbies():
 def except_msg():
     print("Something went wrong.")
 
-Menu().start_loop()
+start_loop()
 
 # SELECT ArtistName, AlbumName 
 # FROM Artists AS a 
