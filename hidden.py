a610fb69bce202f25a04f6c10f6ace618b451070
IsabelValijani <112485206+IsabelValijani@users.noreply.github.com>
2022-09-25 12:26:59 +0200

    Update hidden.py

--- hidden.py
+++ hidden.py
@@ -1,3 +1,5 @@
+#We created a table called "Persons". It contains four columns: PersonID, FirstName, LastName, Birth and Address.
+#The PRIMARY KEY uniquely IDentifies each item in a table with a unique INTEGER value.
 CREATE_TABLE_PERSON = """
                 CREATE TABLE IF NOT EXISTS person(
                     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -8,6 +10,8 @@ CREATE_TABLE_PERSON = """
                 )
                 """
 
+# New table called "Hobbies".
+# The PRIMARY KEY is a clone from the "Persons" table, so that each hobby is linked to the same individual. 
 CREATE_TABLE_HOBBIES = """
                 CREATE TABLE IF NOT EXISTS hobby(
                     hobbyid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -16,6 +20,7 @@ CREATE_TABLE_HOBBIES = """
                 FOREIGN KEY(personid) REFERENCES person(id)
                 )"""
 
+# INSERT DATA -  creates and puts to use the "persons" table in our database. 
 INSERT_PERSON_DATA = """
                 INSERT INTO person(
                     firstname,
@@ -25,7 +30,7 @@ INSERT_PERSON_DATA = """
                 )
                 VALUES (?, ?, ?, ?)
                 """
-
+# INSERT DATA - Creates and operates "hobbies" in our database.
 INSERT_HOBBY_DATA = """
             INSERT INTO hobby(
                 personid,
@@ -34,6 +39,7 @@ INSERT_HOBBY_DATA = """
             VALUES (?, ?)
             """
 
+# Saves the text as a variable to be used later.
 menu_text = """
 
     1. Load data from file
@@ -46,6 +52,7 @@ menu_text = """
 
     """
 
+# Saves the text as a variable to be used later.
 menu2_text = """
 
 What do you want to print for
